170562c0fe7d3bc19e228cf6f64d03ce51e7a5b7
Ihsanuddin <ihsanuddin.sy@gmail.com>
2025-01-07 13:24:56 +0700

    persiapan awal project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+    <h1>new project</h1>
+    <h1>new project</h1>
+    <h1>new project</h1>
+    </>
+  )
+}
+
+export default App
